5412549121655cb018eb6a19300c9e5fa61c0979
randyrichards <randy.richards@columbiacu.org>
2020-07-06 10:07:19 -0700

    Fixing step 3 error

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
-        public ApplicationDbContext(DbContextOptions options): base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
             
         }
